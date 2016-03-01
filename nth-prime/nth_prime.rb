68b43f5adc3d08d2f52f54d15ae2d71e042f25ff
Archana Raut <rautarchana9@gmail.com>
2016-03-01 12:24:21 +0530

    adds the nth prime excercise

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,4 +1,16 @@
 class Prime
-  def nth
-  end  
+  def self.nth(count)
+    raise ArgumentError if count < 1
+    array_of_primes = (2..104750).to_a.select {|y| Prime.is_prime(y)}
+    array_of_primes[count-1]
+  end 
+  def self.is_prime(y)
+    return false if y <= 1
+    2.upto(Math.sqrt(y).to_i)do |n|
+      return false if (y % n).zero?
+    end
+    true
+  end
 end
+p Prime.nth(10001)
+
